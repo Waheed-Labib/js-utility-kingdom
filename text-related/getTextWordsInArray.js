57dbf86121eea33177074be5fd9c5f3c74ac7ca0
Waheed-Labib <labibwahid28@gmail.com>
2025-01-06 11:42:58 +0600

    added getTextWordsInArray

---
+++ text-related/getTextWordsInArray.js
@@ -0,0 +1,7 @@
+// takes a text as parameter
+// returns all the words of the text separated as array elements
+
+const getTextWordsInArray = (text) => {
+    const textWordsInArray = text.split(' ');
+    return textWordsInArray;
+}
